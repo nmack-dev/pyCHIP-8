11691d297f60ca0d9aea349cb9fe228e32560542
nmack <nathan@macktribe.com>
2023-05-21 11:21:36 -0700

    Added input checking on MemObj. Created AddrStack class. Added more tests.

--- chip8/memory.py
+++ chip8/memory.py
@@ -1,18 +1,38 @@
+import functools
+
 class MemObj:
 
-    def __init__(self, size):
+    def __init__(self, size, byte_width):
         self.memory = [0] * size
+        self.byte_width = byte_width
+        self.size = size
 
+
+    def check_memory_size(func):
+        @functools.wraps(func)
+        def wrapper(self, *args):
+            if (int(args[0], 16) <= (self.size - 1)):
+                return func(self, *args)
+            else:
+                raise Exception('ERROR: Out of bounds memory access')
+        return wrapper
+    
+
+    @check_memory_size
     def mem_get(self, location):
         loc = int(location, 16)
         return hex(self.memory[loc])
     
+
+    @check_memory_size
     def mem_set(self, location, val):
         loc = int(location, 16)
         self.memory[loc] = int(val, 16)
 
+
+    @check_memory_size
     def bit_set(self, location, bit, val):
-        if (bit > 7):
+        if (bit > ((self.byte_width * 8) - 1)):
             raise Exception('Can only index bits of byte from 0-7 (8 bits)')
         
         loc = int(location, 16)
@@ -21,18 +41,37 @@ class MemObj:
         bin_val[bit + 3] = val
 
 
+class AddrStack(MemObj):
+
+    def __init__(self, size):
+        self.memory = []
+        self.byte_width = 2
+        self.size = size
+
+    
+    def push_addr(self, addr):
+        if (len(self.memory) < self.size):
+            self.memory.append(addr)
+        else:
+            raise Exception('Stack is full, cannot push address')
+
+    
+    def pop_addr(self):
+        return self.memory.pop()
+
+
 class Memory:
     ram = 4096
     registers = 16
 
     def __init__(self):
-        self.memory = MemObj(self.ram)
+        self.memory = MemObj(self.ram, 1)
         
         self.program_counter = '0x0000'
         
-        self.registers = MemObj(16)
+        self.registers = MemObj(self.registers, 1)
 
-        self.index_reg = '0x0000'
+        self.index_reg = MemObj(1, 2)
         
         self.addr_stack = []
         self.stack_ptr = 0
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -5,10 +5,13 @@ from chip8.memory import MemObj
 class TestMemory(unittest.TestCase):
 
     def test_mem_get(self):
-        mem = MemObj(64)
+        mem = MemObj(64, 1)
 
         self.assertEqual(mem.mem_get('0x0004'), '0x0')
-
+        
+        # Ensures we don't access memory we don't have. 
+        with self.assertRaises(Exception) as context:
+            mem.mem_get('0xFFFF')
 
 if __name__ == '__main__':
     unittest.main()
